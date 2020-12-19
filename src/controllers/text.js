6a6c4f78bf99770eae464ce3cea84bce8dc9842a
Philip Purwoko <philippurwoko123@gmail.com>
2020-12-19 12:31:17 +0700

    Add constlog

--- src/controllers/text.js
+++ src/controllers/text.js
@@ -12,19 +12,22 @@ module.exports = async function HandleMessage(context) {
         const message = context.event.text;
         if (message === 'covid'){
             getAxios('https://covid19.mathdro.id/api/countries/Indonesia').then(stat=>{
-            const json = flex(
-                new Date(stat.lastUpdate),
-                parseInt(stat.confirmed.value),
-                parseInt(stat.recovered.value),
-                parseInt(stat.deaths.value)
-            )
-
-            await context.sendFlex('Statistik Covid 19 Indonesia',json);
-        })
+                console.log('Covid message')
+                const json = flex(
+                    new Date(stat.lastUpdate),
+                    parseInt(stat.confirmed.value),
+                    parseInt(stat.recovered.value),
+                    parseInt(stat.deaths.value)
+                )
+                console.log(json)
+                await context.sendFlex('Statistik Covid 19 Indonesia',json)
+            })
         } else {
+            console.log(`Your Message : ${message}`)
             await context.sendText(`Your Message : ${message}`);
         }
     } else {
+        console.log('Non text message')
         await context.sendText('Maaf, kami hanya bisa memproses pesan Anda. Terima kasih');
     }
 }
