8a754be8b68dfedcd0ea8069a79f32873c8a45a1
Philip Purwoko <philippurwoko123@gmail.com>
2020-12-19 12:53:57 +0700

    Static response

--- src/controllers/text.js
+++ src/controllers/text.js
@@ -11,104 +11,105 @@ module.exports = async function HandleMessage(context) {
     if (context.event.isText){
         const message = context.event.text;
         if (message === 'covid'){
-            await context.sendFlex('Statistik Covid 19 Indonesia',"type":"bubble",
-            "body":{
-                "type":"box",
-                "layout":"vertical",
-                "contents":[
-                    {
-                        "type":"text",
-                        "text":"Data Covid19 Indonesia",
-                        "weight":"bold",
-                        "size":"xl",
-                        "align":"center"
-                    },
-                    {
-                        "type":"box",
-                        "layout":"vertical",
-                        "contents":[
-                            {
-                                "type":"text",
-                                "text":"10-10-2010"
-                            }
-                        ],
-                        "alignItems":"center"
-                    },
-                    {
-                        "type":"box",
-                        "layout":"vertical",
-                        "margin":"lg",
-                        "spacing":"sm",
-                        "contents":[
-                            {
-                                "type":"box",
-                                "layout":"baseline",
-                                "spacing":"sm",
-                                "contents":[
-                                    {
-                                        "type":"text",
-                                        "text":"Total Positif",
-                                        "color":"#aaaaaa",
-                                        "size":"sm"
-                                        // "flex":4
-                                    },
-                                    {
-                                        "type":"text",
-                                        "text":"600000",
-                                        "wrap":true,
-                                        "color":"#666666",
-                                        "size":"sm"
-                                        // "flex":4
-                                    }
-                                ]
-                            },
-                            {
-                                "type":"box",
-                                "layout":"baseline",
-                                "spacing":"sm",
-                                "contents":[
-                                    {
-                                        "type":"text",
-                                        "text":"Total Sembuh",
-                                        "color":"#aaaaaa",
-                                        "size":"sm"
-                                        // "flex":4
-                                    },
-                                    {
-                                        "type":"text",
-                                        "text":"25000",
-                                        "wrap":true,
-                                        "color":"#666666",
-                                        "size":"sm"
-                                        // "flex":4
-                                    }
-                                ]
-                            },
-                            {
-                                "type":"box",
-                                "layout":"baseline",
-                                "contents":[
-                                    {
-                                        "type":"text",
-                                        "text":"Total Meninggal",
-                                        // "flex":4,
-                                        "size":"sm",
-                                        "color":"#aaaaaa"
-                                    },
-                                    {
-                                        "type":"text",
-                                        "text":"3000",
-                                        // "flex":4,
-                                        "size":"sm",
-                                        "color":"#666666"
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ]
-            }
-        })
+            await context.sendFlex('Statistik Covid 19 Indonesia',{
+                "type":"bubble",
+                "body":{
+                    "type":"box",
+                    "layout":"vertical",
+                    "contents":[
+                        {
+                            "type":"text",
+                            "text":"Data Covid19 Indonesia",
+                            "weight":"bold",
+                            "size":"xl",
+                            "align":"center"
+                        },
+                        {
+                            "type":"box",
+                            "layout":"vertical",
+                            "contents":[
+                                {
+                                    "type":"text",
+                                    "text":"10-10-2010"
+                                }
+                            ],
+                            "alignItems":"center"
+                        },
+                        {
+                            "type":"box",
+                            "layout":"vertical",
+                            "margin":"lg",
+                            "spacing":"sm",
+                            "contents":[
+                                {
+                                    "type":"box",
+                                    "layout":"baseline",
+                                    "spacing":"sm",
+                                    "contents":[
+                                        {
+                                            "type":"text",
+                                            "text":"Total Positif",
+                                            "color":"#aaaaaa",
+                                            "size":"sm"
+                                            // "flex":4
+                                        },
+                                        {
+                                            "type":"text",
+                                            "text":"600000",
+                                            "wrap":true,
+                                            "color":"#666666",
+                                            "size":"sm"
+                                            // "flex":4
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type":"box",
+                                    "layout":"baseline",
+                                    "spacing":"sm",
+                                    "contents":[
+                                        {
+                                            "type":"text",
+                                            "text":"Total Sembuh",
+                                            "color":"#aaaaaa",
+                                            "size":"sm"
+                                            // "flex":4
+                                        },
+                                        {
+                                            "type":"text",
+                                            "text":"25000",
+                                            "wrap":true,
+                                            "color":"#666666",
+                                            "size":"sm"
+                                            // "flex":4
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type":"box",
+                                    "layout":"baseline",
+                                    "contents":[
+                                        {
+                                            "type":"text",
+                                            "text":"Total Meninggal",
+                                            // "flex":4,
+                                            "size":"sm",
+                                            "color":"#aaaaaa"
+                                        },
+                                        {
+                                            "type":"text",
+                                            "text":"3000",
+                                            // "flex":4,
+                                            "size":"sm",
+                                            "color":"#666666"
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            })
         } else {
             console.log(`Your Message : ${message}`)
             await context.sendText(`Your Message : ${message}`);
