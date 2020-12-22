5464a81cfa9ab2a9bac1ce04fa253d57c22ddcfa
Philip Purwoko <philippurwoko123@gmail.com>
2020-12-22 16:38:01 +0700

    :sparkles: Enhance covid flex message

--- src/messages/covid.js
+++ src/messages/covid.js
@@ -1,8 +1,19 @@
-module.exports = (tanggalRaw, positif, sembuh, meninggal) => {
+function threeDot(value){
+    return value.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.");
+}
+
+
+module.exports = (tanggalRaw, positifRaw, sembuh, meninggal) => {
+    // Format raw datetime string
     const date_time = tanggalRaw.split('T');
     const date = date_time[0].split('-').reverse().join('-')
     const time = date_time[1].split(':').slice(0,2).join(':')
     const tanggal = `Tanggal ${date} Jam ${time}`
+
+    // Add . every 3 digit from behind
+    const positif = threeDot(positifRaw);
+    const sembuh = threeDot(sembuhRaw);
+    const meninggal = threeDot(meninggalRaw);
     
     return {
         "type": "bubble",
